730d7de997b13ea43dded7b6c718a91dc91cdad2
kamuli3t <kamuli3t@gmail.com>
2025-03-26 18:32:43 -0700

    add Tailwind CSS integration and update HomePage layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,13 @@ import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <div>
+    <>
       <header>Header here</header>
       <main>
         <Outlet />
       </main>
       <footer>Footer here</footer>
-    </div>
+    </>
   );
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <div></div>
+      <div>
+        <h1>Kaml</h1>
+        <h3>Mehamednur</h3>
+      </div>
+    </>
+  );
 }
 
 export default HomePage;
